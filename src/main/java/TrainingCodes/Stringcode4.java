1a9f8eb4e8596e54360bb7f88031c37d7c847243
Samarth Hatture <hatturesamarth@gmail.com>
2023-03-28 22:42:41 +0530

    Rolling String

---
+++ src/main/java/TrainingCodes/Stringcode4.java
@@ -0,0 +1,17 @@
+package TrainingCodes;
+
+public class Stringcode4 {
+
+
+	public static void main(String[] args) 
+			throws InterruptedException {
+		String str=new String("SAMARTH HATTURE");
+		for(int i=0;i<str.length();i++) {
+			System.out.print(str.charAt(i));
+			Thread.sleep(650);
+		}
+			
+
+	}
+
+}
